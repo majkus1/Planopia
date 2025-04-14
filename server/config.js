aeaed7466f5168f366bfcc41cd23ea6bcbf05d00
MichalLipka867 <lipkam420@gmail.com>
2025-04-14 17:39:23 +0200

    new frontend address

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 // server/config.js
 const appUrl =
 	process.env.NODE_ENV === 'production'
-		? 'https://planopia.pl'
+		? 'https://sprightly-centaur-b4cdcd.netlify.app'
 		: 'http://localhost:3001'
 
 module.exports = {
