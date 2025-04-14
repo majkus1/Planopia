d7c1b112d045d85cab78a37ce7ba026e8252e2d5
MichalLipka867 <lipkam420@gmail.com>
2025-04-14 17:32:38 +0200

    new backend address

--- client/src/config.jsx
+++ client/src/config.jsx
@@ -2,5 +2,5 @@
 //   (process.env.NODE_ENV === 'production' ? 'https://api.planopia.pl' : 'http://localhost:3000');
 export const API_URL = import.meta.env.VITE_API_URL || 
   (import.meta.env.MODE === 'production'
-    ? 'https://api.planopia.pl'
+    ? 'https://planopia.onrender.com'
     : 'http://localhost:3000');
