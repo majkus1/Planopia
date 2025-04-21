6183d4faf29bbdadba37dfe19acbab689855e149
MichalLipka867 <lipkam420@gmail.com>
2025-04-21 20:08:44 +0200

    change refresh token

--- client/src/axiosSetup.js
+++ client/src/axiosSetup.js
@@ -1,3 +1,36 @@
+// import axios from 'axios'
+// import { API_URL } from './config.js'
+
+// axios.defaults.withCredentials = true // ważne do obsługi cookie
+
+// // Prefetch CSRF token
+// const fetchCsrfToken = async () => {
+//   try {
+//     const res = await axios.get(`${API_URL}/api/csrf-token`, { withCredentials: true });
+//     axios.defaults.headers.common['X-CSRF-Token'] = res.data.csrfToken;
+//   } catch (err) {
+//     console.error('CSRF prefetch error', err);
+//   }
+// };
+
+// fetchCsrfToken();
+
+// // Interceptor
+// axios.interceptors.request.use(async config => {
+//   const method = config.method?.toLowerCase();
+//   const needsCsrf = ['post', 'put', 'delete', 'patch'].includes(method);
+
+//   if (needsCsrf && !axios.defaults.headers.common['X-CSRF-Token']) {
+//     try {
+//       const res = await axios.get(`${API_URL}/api/csrf-token`, { withCredentials: true });
+//       axios.defaults.headers.common['X-CSRF-Token'] = res.data.csrfToken;
+//     } catch (err) {
+//       console.error('CSRF fetch error:', err);
+//     }
+//   }
+
+//   return config;
+// }, error => Promise.reject(error));
 import axios from 'axios'
 import { API_URL } from './config.js'
 
@@ -15,7 +48,7 @@ const fetchCsrfToken = async () => {
 
 fetchCsrfToken();
 
-// Interceptor
+// Request Interceptor - CSRF token
 axios.interceptors.request.use(async config => {
   const method = config.method?.toLowerCase();
   const needsCsrf = ['post', 'put', 'delete', 'patch'].includes(method);
@@ -31,3 +64,52 @@ axios.interceptors.request.use(async config => {
 
   return config;
 }, error => Promise.reject(error));
+
+// Response Interceptor - Refresh token logic
+let isRefreshing = false
+let failedQueue = []
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error)
+    } else {
+      prom.resolve(token)
+    }
+  })
+
+  failedQueue = []
+}
+
+axios.interceptors.response.use(
+  res => res,
+  async err => {
+    const originalRequest = err.config
+
+    if (err.response?.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise(function (resolve, reject) {
+          failedQueue.push({ resolve, reject })
+        })
+          .then(() => axios(originalRequest))
+          .catch(error => Promise.reject(error))
+      }
+
+      originalRequest._retry = true
+      isRefreshing = true
+
+      try {
+        await axios.post(`${API_URL}/api/users/refresh-token`, {}, { withCredentials: true })
+        processQueue(null)
+        return axios(originalRequest)
+      } catch (refreshError) {
+        processQueue(refreshError, null)
+        return Promise.reject(refreshError)
+      } finally {
+        isRefreshing = false
+      }
+    }
+
+    return Promise.reject(err)
+  }
+)
