48b78e3092ba19927bd724c2ced480ac82fef6cf
majkus1 <lipkam420@gmail.com>
2025-08-30 00:53:41 +0200

    add new email

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -5,7 +5,7 @@ const { appUrl } = require('../config')
 
 const sendEmail = async (to, link, subject, html) => {
 	const transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
+		host: 'smtp.hostinger.com',
 		port: 465,
 		secure: true,
 		auth: {
@@ -15,7 +15,7 @@ const sendEmail = async (to, link, subject, html) => {
 	})
 
 	const info = await transporter.sendMail({
-		from: '"planopia.pl" <your-email@gmail.com>',
+		from: '"Planopia" <aplikacja@planopia.pl>',
 		to,
 		subject,
 		html,
